be3d3a285189d99f9a3a05be9cdc46e4733a2022
ElenaVyshniak <138595658+ElenaVyshniak@users.noreply.github.com>
2023-11-28 16:06:40 +0200

    Update index.js

--- src/src/index.js
+++ src/src/index.js
@@ -1,27 +1,41 @@
-import axios from 'axios';
+const breedSelect = document.querySelector('.breed-select');
+const loader = document.querySelector('.loader');
+const error = document.querySelector('.error');
+const catInfo = document.querySelector('.cat-info');
 
-axios.defaults.headers.common['x-api-key'] =
-  'live_FyckgQNH5X5BXMHuuWj8EgkfY7FcewAgVs2OZscIRCnSDI12cNmbH8Fu9cxZ5GpP';
-const urlBreeds = 'https://api.thecatapi.com/v1/breeds';
+const { fetchBreeds, fetchCatByBreed } = require('./cat-api.js');
 
-function fetchBreeds() {
-  return axios
-    .get(urlBreeds)
-    .then(response => response.data)
+fetchBreeds()
+  .then(breeds => {
+    for (let i = 0; i < breeds.length; i++) {
+      let option = document.createElement('option');
+      option.value = i;
+      option.innerHTML = `${breeds[i].name}`;
+      // console.log(option);
+      breedSelect.appendChild(option);
+    }
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
+breedSelect.addEventListener('change', function () {
+  const selectedBreedId = this.value;
+  catInfo.innerHTML = '';
+
+  fetchCatByBreed(selectedBreedId)
+    .then(breed => {
+      const catImg = document.createElement('img');
+      catImg.src = `${breed.url}`;
+      const catTitle = document.createElement('h2');
+      catTitle.textContent = `${breed.name}`;
+      const catText = document.createElement('p');
+      catText.textContent = `${breed.temperament}`;
+      catInfo.appendChild(catImg);
+      catInfo.appendChild(catTitle);
+      catInfo.appendChild(catText);
+    })
     .catch(error => {
-      console.log(error);
-      throw error;
+      console.error('Error fetching cat by breed:', error);
     });
-}
-
-function fetchCatByBreed(breedId) {
-  return axios
-    .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
-    .then(response => response.data)
-    .catch(error => {
-      console.log(error);
-      throw error;
-    });
-}
-
-module.exports = { fetchBreeds, fetchCatByBreed };
+});
