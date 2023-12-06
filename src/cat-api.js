e56ba0f6f3b5e9f54fdac1076650f59ae72d8333
ElenaVyshniak <138595658+ElenaVyshniak@users.noreply.github.com>
2023-12-06 18:39:12 +0200

    ffff

---
+++ src/cat-api.js
@@ -0,0 +1,27 @@
+// import axios from 'axios';
+
+// axios.defaults.headers.common['x-api-key'] =
+//   'live_n2ND2q7UjmqCtzYqN0M9RY9AeEzLxLRhEVGqjnzVrmKV3FgoycYwhhnphz2KTV3x';
+
+const API_KEY =
+  'live_V8HYuwQGHrCRuDbKWADV94hb3cIE0fKwbJIkP3ilFvA8HvXoQWdO7ytqkZcSG1Wg';
+const BREED_URL = 'https://api.thecatapi.com/v1/breeds?api_key';
+const IMG_URL = 'https://api.thecatapi.com/v1/images/search?api_key';
+
+export const fetchBreeds = () => {
+  return fetch(`${BREED_URL}=${API_KEY}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    return response.json();
+  });
+};
+
+export const fetchCatByBreed = breedId => {
+  return fetch(`${IMG_URL}=${API_KEY}&breed_ids=${breedId}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    return response.json();
+  });
+};
