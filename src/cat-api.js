266044b47a46c8ea3ba4b1353a93d05febec5618
ElenaVyshniak <138595658+ElenaVyshniak@users.noreply.github.com>
2023-12-08 21:34:02 +0200

    jjjj

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,20 +1,22 @@
-import axios from 'axios';
-
-axios.defaults.headers.common['x-api-key'] =
+const api_key =
   'live_n2ND2q7UjmqCtzYqN0M9RY9AeEzLxLRhEVGqjnzVrmKV3FgoycYwhhnphz2KTV3x';
-const apiUrl = 'https://api.thecatapi.com/v1/breeds';
 
 export function fetchBreeds() {
-  return axios
-    .get(apiUrl)
+  const apiUrl = 'https://api.thecatapi.com/v1/breeds';
+
+  return fetch(apiUrl)
     .then(response => {
-      return response.data.map(breed => ({
+      if (!response.ok) {
+        throw new Error(
+          `Failed to fetch breeds. Status: ${response.status} - ${response.statusText}`
+        );
+      }
+      return response.json();
+    })
+    .then(breeds => {
+      return breeds.map(breed => ({
         id: breed.id,
         name: breed.name,
       }));
-    })
-    .catch(error => {
-      console.error('Failed to fetch breeds:', error);
-      throw error; // Ретранслювати помилку, щоб обробити її викликаючим кодом
     });
 }
