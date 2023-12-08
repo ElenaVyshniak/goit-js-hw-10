af95f2a2d3039eea87b3fdbed5cebc5f6490d877
ElenaVyshniak <138595658+ElenaVyshniak@users.noreply.github.com>
2023-12-08 21:22:45 +0200

    nnnn

--- src/cat-api.js
+++ src/cat-api.js
@@ -2,26 +2,19 @@ import axios from 'axios';
 
 axios.defaults.headers.common['x-api-key'] =
   'live_n2ND2q7UjmqCtzYqN0M9RY9AeEzLxLRhEVGqjnzVrmKV3FgoycYwhhnphz2KTV3x';
-const url = 'https://api.thecatapi.com/v1/breeds';
-// const api_key =
-//   'live_n2ND2q7UjmqCtzYqN0M9RY9AeEzLxLRhEVGqjnzVrmKV3FgoycYwhhnphz2KTV3x';
+const apiUrl = 'https://api.thecatapi.com/v1/breeds';
 
 export function fetchBreeds() {
-  const apiUrl = 'https://api.thecatapi.com/v1/breeds';
-
-  return fetch(apiUrl)
+  return axios
+    .get(apiUrl)
     .then(response => {
-      if (!response.ok) {
-        throw new Error(
-          `Failed to fetch breeds. Status: ${response.status} - ${response.statusText}`
-        );
-      }
-      return response.json();
-    })
-    .then(breeds => {
-      return breeds.map(breed => ({
+      return response.data.map(breed => ({
         id: breed.id,
         name: breed.name,
       }));
+    })
+    .catch(error => {
+      console.error('Failed to fetch breeds:', error);
+      throw error; // Ретранслювати помилку, щоб обробити її викликаючим кодом
     });
 }
--- src/index.js
+++ src/index.js
@@ -4,71 +4,43 @@ import 'slim-select/dist/slimselect.css';
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
 import './css/stylle.css';
 
-import { fetchBreeds } from './cat-api';
-import { fetchCatByBreed } from './cat-api';
-
 const body = document.querySelector('body');
 const select = document.querySelector('.breed-select');
 const loader = document.querySelector('.loader');
 const error = document.querySelector('.error');
 const div = document.querySelector('.cat-info');
 
-// document.addEventListener('DOMContentLoaded', async () => {
-//   const breeds = await fetchBreeds();
-//   const breedSelect = document.getElementById('breedSelect');
-//   const catInfoDiv = document.getElementById('catInfo');
+document.addEventListener('DOMContentLoaded', async () => {
+  try {
+    const breeds = await fetchBreeds();
 
-//   breeds.forEach(breed => {
-//     const option = document.createElement('option');
-//     option.value = breed.id;
-//     option.textContent = breed.name;
-//     breedSelect.appendChild(option);
-//   });
+    new SlimSelect({
+      select: '.breed-select',
+      placeholder: 'Select a breed',
+      allowDeselect: true,
+      onChange: async info => {
+        const selectedBreedId = info.value();
+        const catData = await fetchCatByBreed(selectedBreedId);
 
-// breedSelect.addEventListener('change', async () => {
-//   const selectedBreedId = breedSelect.value;
-//   const catData = await fetchCatByBreed(selectedBreedId);
+        div.innerHTML = `
+          <img src="${catData.url}" alt="Cat Image">
+          <p>Breed: ${catData.breeds[0].name}</p>
+          <p>Description: ${catData.breeds[0].description}</p>
+          <p>Temperament: ${catData.breeds[0].temperament}</p>
+        `;
+      },
+    });
 
-//   catInfoDiv.innerHTML = `
-//           <img src="${catData.url}" alt="Cat Image">
-//           <p>Breed: ${catData.breeds[0].name}</p>
-//           <p>Description: ${catData.breeds[0].description}</p>
-//           <p>Temperament: ${catData.breeds[0].temperament}</p>
-//       `;
-// });
-
-// const breedId = event.target.value;
-// fetchCatByBreed(breedId)
-//   .then(data => {
-//     info.innerHTML = '';
-//     const { url, breeds } = data[0];
-//     const markup = `
-//         <div class="box-img">
-//         <img src="${url}" alt="${breeds[0].name}" width="400"/>
-//         </div><div class="box">
-//         <h1>${breeds[0].name}</h1>
-//         <p>${breeds[0].description}</p>
-//         <p>
-//         <b>Temperament:</b>
-//         ${breeds[0].temperament}</p>
-//         </div>`;
-//     info.insertAdjacentHTML('beforeend', markup);
-//   })
-//   .catch(onFetchError);
-
-// function onFetchError() {
-//   setLoadingState(true);
-//   Notify.failure(errorMessage.textContent, {
-//     position: 'center-center',
-//     timeout: 5000,
-//     width: '600px',
-//     fontSize: '34px',
-//   });
-// };
-
-// const breedAndFetch = () => {
-//   fetchBreeds()
-//     .then(breeds => renderOptions(breeds))
-//     .catch(error => err());
-// };
-// breedAndFetch();
+    breeds.forEach(breed => {
+      select.appendChild(new Option(breed.name, breed.id));
+    });
+  } catch (error) {
+    console.error('Error fetching breeds:', error);
+    Notify.failure('Failed to fetch breeds', {
+      position: 'center-center',
+      timeout: 5000,
+      width: '600px',
+      fontSize: '34px',
+    });
+  }
+});
