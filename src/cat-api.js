93ce3e2fe82ae78042b6149b2e0f3441d3d9d63e
ElenaVyshniak <138595658+ElenaVyshniak@users.noreply.github.com>
2023-12-08 22:30:30 +0200

    crazy

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,28 +1,22 @@
-// Constants
 const api_key =
   'live_n2ND2q7UjmqCtzYqN0M9RY9AeEzLxLRhEVGqjnzVrmKV3FgoycYwhhnphz2KTV3x';
 
-// Fetch breeds from the Cat API
-export async function fetchBreeds() {
+export function fetchBreeds() {
   const apiUrl = 'https://api.thecatapi.com/v1/breeds';
 
-  try {
-    const response = await fetch(apiUrl);
-
-    if (!response.ok) {
-      throw new Error(
-        `Failed to fetch breeds. Status: ${response.status} - ${response.statusText}`
-      );
-    }
-
-    const breeds = await response.json();
-
-    return breeds.map(breed => ({
-      id: breed.id,
-      name: breed.name,
-    }));
-  } catch (error) {
-    console.error('Error fetching breeds:', error);
-    throw error; // Re-throw the error to let the caller handle it
-  }
+  return fetch(apiUrl)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(
+          `Failed to fetch breeds. Status: ${response.status} - ${response.statusText}`
+        );
+      }
+      return response.json();
+    })
+    .then(breeds => {
+      return breeds.map(breed => ({
+        id: breed.id,
+        name: breed.name,
+      }));
+    });
 }
