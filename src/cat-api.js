04fddafb3695d2c7ea69832cad9c19333b11e99f
ElenaVyshniak <138595658+ElenaVyshniak@users.noreply.github.com>
2023-12-08 22:22:24 +0200

    еуууу

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,34 +1,14 @@
-// const api_key =
-//   'live_n2ND2q7UjmqCtzYqN0M9RY9AeEzLxLRhEVGqjnzVrmKV3FgoycYwhhnphz2KTV3x';
-
-// export function fetchBreeds() {
-//   const apiUrl = 'https://api.thecatapi.com/v1/breeds';
-
-//   return fetch(apiUrl)
-//     .then(response => {
-//       if (!response.ok) {
-//         throw new Error(
-//           `Failed to fetch breeds. Status: ${response.status} - ${response.statusText}`
-//         );
-//       }
-//       return response.json();
-//     })
-//     .then(breeds => {
-//       return breeds.map(breed => ({
-//         id: breed.id,
-//         name: breed.name,
-//       }));
-//     });
-// }
-
-const apiKey =
+// Constants
+const api_key =
   'live_n2ND2q7UjmqCtzYqN0M9RY9AeEzLxLRhEVGqjnzVrmKV3FgoycYwhhnphz2KTV3x';
 
+// Fetch breeds from the Cat API
 export async function fetchBreeds() {
   const apiUrl = 'https://api.thecatapi.com/v1/breeds';
 
   try {
     const response = await fetch(apiUrl);
+
     if (!response.ok) {
       throw new Error(
         `Failed to fetch breeds. Status: ${response.status} - ${response.statusText}`
@@ -36,29 +16,13 @@ export async function fetchBreeds() {
     }
 
     const breeds = await response.json();
+
     return breeds.map(breed => ({
       id: breed.id,
       name: breed.name,
     }));
   } catch (error) {
-    throw error;
-  }
-}
-
-export async function fetchCatByBreed(breedId) {
-  const apiUrl = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`;
-
-  try {
-    const response = await fetch(apiUrl);
-    if (!response.ok) {
-      throw new Error(
-        `Failed to fetch cat by breed. Status: ${response.status} - ${response.statusText}`
-      );
-    }
-
-    const catData = await response.json();
-    return catData[0];
-  } catch (error) {
+    console.error('Error fetching breeds:', error);
     throw error;
   }
 }
