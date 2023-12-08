325fc75081d1697996c9236cf7c6d7eb3817a002
ElenaVyshniak <138595658+ElenaVyshniak@users.noreply.github.com>
2023-12-08 21:53:16 +0200

    dddd

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,22 +1,64 @@
-const api_key =
+// const api_key =
+//   'live_n2ND2q7UjmqCtzYqN0M9RY9AeEzLxLRhEVGqjnzVrmKV3FgoycYwhhnphz2KTV3x';
+
+// export function fetchBreeds() {
+//   const apiUrl = 'https://api.thecatapi.com/v1/breeds';
+
+//   return fetch(apiUrl)
+//     .then(response => {
+//       if (!response.ok) {
+//         throw new Error(
+//           `Failed to fetch breeds. Status: ${response.status} - ${response.statusText}`
+//         );
+//       }
+//       return response.json();
+//     })
+//     .then(breeds => {
+//       return breeds.map(breed => ({
+//         id: breed.id,
+//         name: breed.name,
+//       }));
+//     });
+// }
+
+const apiKey =
   'live_n2ND2q7UjmqCtzYqN0M9RY9AeEzLxLRhEVGqjnzVrmKV3FgoycYwhhnphz2KTV3x';
 
-export function fetchBreeds() {
+export async function fetchBreeds() {
   const apiUrl = 'https://api.thecatapi.com/v1/breeds';
 
-  return fetch(apiUrl)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(
-          `Failed to fetch breeds. Status: ${response.status} - ${response.statusText}`
-        );
-      }
-      return response.json();
-    })
-    .then(breeds => {
-      return breeds.map(breed => ({
-        id: breed.id,
-        name: breed.name,
-      }));
-    });
+  try {
+    const response = await fetch(apiUrl);
+    if (!response.ok) {
+      throw new Error(
+        `Failed to fetch breeds. Status: ${response.status} - ${response.statusText}`
+      );
+    }
+
+    const breeds = await response.json();
+    return breeds.map(breed => ({
+      id: breed.id,
+      name: breed.name,
+    }));
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function fetchCatByBreed(breedId) {
+  const apiUrl = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`;
+
+  try {
+    const response = await fetch(apiUrl);
+    if (!response.ok) {
+      throw new Error(
+        `Failed to fetch cat by breed. Status: ${response.status} - ${response.statusText}`
+      );
+    }
+
+    const catData = await response.json();
+    return catData[0];
+  } catch (error) {
+    throw error;
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,46 +1,114 @@
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// import SlimSelect from 'slim-select';
+// import 'slim-select/dist/slimselect.css';
+// import { fetchBreeds, fetchCatByBreed } from './cat-api';
+// import './css/stylle.css';
+
+// // const body = document.querySelector('body');
+// const select = document.querySelector('.breed-select');
+// const Loader = document.querySelector('.loader');
+// const Error = document.querySelector('.error');
+// const CatInfo = document.querySelector('.cat-info');
+
+// document.addEventListener('DOMContentLoaded', async () => {
+//   try {
+//     const breeds = await fetchBreeds();
+
+//     new SlimSelect({
+//       select: '.breed-select',
+//       placeholder: 'Select a breed',
+//       allowDeselect: true,
+//       onChange: async info => {
+//         const selectedBreedId = info.value();
+//         const catData = await fetchCatByBreed(selectedBreedId);
+
+//         div.innerHTML = `
+//           <img src="${catData.url}" alt="Cat Image">
+//           <p>Breed: ${catData.breeds[0].name}</p>
+//           <p>Description: ${catData.breeds[0].description}</p>
+//           <p>Temperament: ${catData.breeds[0].temperament}</p>
+//         `;
+//       },
+//     });
+
+//     breeds.forEach(breed => {
+//       select.appendChild(new Option(breed.name, breed.id));
+//     });
+//   } catch (error) {
+//     console.error('Error fetching breeds:', error);
+//     Notify.failure('Failed to fetch breeds', {
+//       position: 'center-center',
+//       timeout: 5000,
+//       width: '600px',
+//       fontSize: '34px',
+//     });
+//   }
+// });
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SlimSelect from 'slim-select';
-import 'slim-select/dist/slimselect.css';
+import './css/style.css';
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
-import './css/stylle.css';
-
-const body = document.querySelector('body');
-const select = document.querySelector('.breed-select');
-const loader = document.querySelector('.loader');
-const error = document.querySelector('.error');
-const div = document.querySelector('.cat-info');
 
 document.addEventListener('DOMContentLoaded', async () => {
-  try {
-    const breeds = await fetchBreeds();
-
-    new SlimSelect({
-      select: '.breed-select',
-      placeholder: 'Select a breed',
-      allowDeselect: true,
-      onChange: async info => {
-        const selectedBreedId = info.value();
+  const breedSelect = new SlimSelect({
+    select: '.breed-select',
+    placeholder: 'Select a breed',
+    allowDeselect: true,
+    onChange: async info => {
+      const selectedBreedId = info.value();
+      try {
+        showLoader();
         const catData = await fetchCatByBreed(selectedBreedId);
+        displayCatInfo(catData);
+      } catch (error) {
+        handleError(error);
+      } finally {
+        hideLoader();
+      }
+    },
+  });
 
-        div.innerHTML = `
-          <img src="${catData.url}" alt="Cat Image">
-          <p>Breed: ${catData.breeds[0].name}</p>
-          <p>Description: ${catData.breeds[0].description}</p>
-          <p>Temperament: ${catData.breeds[0].temperament}</p>
-        `;
-      },
-    });
-
+  try {
+    showLoader();
+    const breeds = await fetchBreeds();
     breeds.forEach(breed => {
-      select.appendChild(new Option(breed.name, breed.id));
+      breedSelect.appendChild(new Option(breed.name, breed.id));
     });
   } catch (error) {
-    console.error('Error fetching breeds:', error);
-    Notify.failure('Failed to fetch breeds', {
-      position: 'center-center',
-      timeout: 5000,
-      width: '600px',
-      fontSize: '34px',
-    });
+    handleError(error);
+  } finally {
+    hideLoader();
   }
 });
+
+function showLoader() {
+  document.querySelector('.loader').style.display = 'block';
+  document.querySelector('.breed-select').style.display = 'none';
+  document.querySelector('.error').style.display = 'none';
+}
+
+function hideLoader() {
+  document.querySelector('.loader').style.display = 'none';
+  document.querySelector('.breed-select').style.display = 'block';
+}
+
+function displayCatInfo(catData) {
+  const catInfo = document.querySelector('.cat-info');
+  catInfo.innerHTML = `
+    <img src="${catData.url}" alt="Cat Image">
+    <p>Breed: ${catData.breeds[0].name}</p>
+    <p>Description: ${catData.breeds[0].description}</p>
+    <p>Temperament: ${catData.breeds[0].temperament}</p>
+  `;
+}
+
+function handleError(error) {
+  console.error('Error:', error);
+  Notify.failure('Failed to fetch data', {
+    position: 'center-center',
+    timeout: 5000,
+    width: '600px',
+    fontSize: '34px',
+  });
+  document.querySelector('.error').style.display = 'block';
+}
